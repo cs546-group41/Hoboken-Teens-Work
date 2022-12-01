c7682504480912bf6fb8e5373381d365ce790912
Divyang <8656786+perseus01@users.noreply.github.com>
2022-11-30 19:38:20 -0500

    Revert "Merge pull request #15 from cs546-group41/dev"
    This reverts commit 4f10110a1153159acdd0463544823b5c1026f1a2, reversing
    changes made to 9de2092d99c373a8dba12aaa5af0c82dfc4fb8bd.

--- data/comments.js
+++
@@ -1,7 +0,0 @@
-const mongoCollections = require("../config/mongoCollection");
-const users = mongoCollections.users;
-const jobs = mongoCollections.jobs;
-const validation = require("../validation");
-const { ObjectId } = require("mongodb");
-const usersData = require("./users");
-const jobsData = require("./jobs");
--- data/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-	users: require("./users"),
-	jobs: require("./jobs"),
-	comments: require("./comments")
-};
