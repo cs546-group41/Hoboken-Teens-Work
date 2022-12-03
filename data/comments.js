a424d8664b80dc1f779c7ada342df9df96cb977b
Divyang <8656786+perseus01@users.noreply.github.com>
2022-12-03 18:59:30 -0500

    - Added 2 functions to data/comments.js     * createComment()     * deleteComment - Removed redundant dependency "handlebars" as "express-handlebars" is already installed

--- data/comments.js
+++ data/comments.js
@@ -5,3 +5,20 @@ const validation = require("../validation");
 const { ObjectId } = require("mongodb");
 const usersData = require("./users");
 const jobsData = require("./jobs");
+
+const createComment = async(jobId, commentorId, comment) => {
+    if(!jobId) throw "Must provide a Job ID";
+    if(!commentorId) throw "Must provide user ID of commentor";
+    if(!comment) throw "Must provide a comment";
+};
+
+const deleteComment = async(jobId, commentorId, commentId) => {
+    if(!jobId) throw "Must provide a Job ID";
+    if(!commentorId) throw "Must provide user ID of commentor";
+    if(!commentId) throw "Must provide ID of comment to be deleted";
+};
+
+module.exports = {
+    createComment,
+    deleteComment
+};
