21f9ecd35f7b957dc94f88a86af9f8c454733bb6
Siddharth <sdalvi3@stevens.edu>
2022-12-10 18:19:52 -0500

    Done with client side validation for login form

--- public/js/loginValidation.js
+++ public/js/loginValidation.js
@@ -1,40 +1,63 @@
-
-
-
-const checkUser = async (email, password) => {
-    const userCollection = await users()
-    const validatedEmail = validation.checkString(email)
-    const validatedPassword = validation.checkPassword(password)
-    let userFound = await userCollection.findOne({ email: validatedEmail })
-    if (userFound === null) throw "Either the email or passwaord is invalid"
-    let userPassword = userFound.hashedPassword
-    let comparePassword = false
-    comparePassword = await bcrypt.compare(validatedPassword, userPassword)
-    if (comparePassword) {
-        return { AuthenticatedUser: true }
-    } else throw "Either the email or password is invalid"
+function checkUserclientSideEmail(email){
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+		email = email.trim();
+		return email;
+	} else {
+		throw "Invalid email format";
+	}
+}
+function checkUserClientSidePassword(strVal){
+    const oneUpper = /[A-Z]/;
+	const oneNumber = /[0-9]/;
+	const specialChar = /[^\w\s]/;
+	if (!strVal) throw "You must supply a password!";
+	if (typeof strVal !== "string") throw "Password must be a string!";
+	strVal = strVal.trim();
+	if (strVal.length === 0) throw "Password cannot be an empty string or string with just spaces";
+	if (strVal.length < 6) throw "Password must at least 6 characters long";
+	if (strVal.includes(" ")) throw "Password must not contain space";
+	if (!oneUpper.test(strVal)) throw "Password must contain one upper case ";
+	if (!oneNumber.test(strVal)) throw "Password must contain one number ";
+	if (!specialChar.test(strVal) && !strVal.includes("_")) throw "Password must contain one special character ";
+	return strVal;
 }
 
 
 const staticForm = document.getElementById("login-form")
-console.log(staticForm)
 
 if (staticForm) {
     const usernameInput = document.getElementById("username")
-    console.log(usernameInput)
 
     const passwordInput = document.getElementById("password")
-    console.log(passwordInput)
-
-    const resultContainer = document.getElementById('result-container')
-    const resultTextElem = resultContainer.getElementsByClassName('text-goes-here')[0];
 
     const errorContainer = document.getElementById('error-container')
     const errorTextElem = document.getElementsByClassName('text-goes-here')[0];
 
     staticForm.addEventListener('submit', (event) => {
         event.preventDefault();
+        try{
+            // resultContainer.classList.add('hidden')
+            errorContainer.classList.add('hidden')
+            const usernameInputElem = usernameInput.value;
+            console.log(usernameInputElem)
+
+            const passwordInputElem = passwordInput.value;
+            console.log(passwordInputElem)
 
 
+            const validatedEmail = checkUserclientSideEmail(usernameInputElem)
+            console.log(validatedEmail)
+            const validatedPassword = checkUserClientSidePassword(passwordInputElem)
+
+            if(validatedEmail){
+                errorContainer.style.display = "none"
+            }
+            if(validatedPassword){
+                errorContainer.style.display = "none"
+            }
+        }catch(e){
+            errorTextElem.textContent = "Error: " + e
+            errorContainer.style.display = "block"
+        }
     })
 }
