53828f3360e87805b404ef2d5a4a95ca140ba68c
Divyang <8656786+perseus01@users.noreply.github.com>
2022-12-13 17:38:23 -0500

    - In data/users     * Fixed createUser function     * Removed redundant functions
    - In data/jobs
        * createJob now includes date of job creation
        * jobSearch converts IDs to strings before returning results

--- seed.js
+++ seed.js
@@ -1,44 +1,97 @@
 const users = require("./data/users");
-const connection = require("./config/mongoConnection");
 const jobs = require("./data/jobs");
+const comments = require("./data/comments");
+const connection = require("./config/mongoConnection");
 const { ObjectId } = require("mongodb");
 
 let firstUser = undefined;
+let secondUser = undefined;
+let thirdUser = undefined;
+let fourthUser = undefined;
+let fifthUser = undefined;
+let sixthUser = undefined;
+let newJob = undefined;
+let newComment = undefined;
 
 async function main() {
 	const db = await connection.dbConnection();
 	await db.dropDatabase();
 
-	// try{
-	//   await jobs.createJob("test","this is a test job","12th St","638add4a89d01f01e5dd77bb")
-	// }catch(e){
-	//   console.log(e)
-	// }
+	console.log("Create Users");
 
 	try {
-		firstUser = await users.createUser("abc", "bcd", "ab@ab.edu", 14, "9465312648", "QWE123!@#");
+		firstUser = await users.createUser("John", "Doe", "johndoe@email.com", 23, "Htw@123", "3482749378");
 		console.log(firstUser);
 	} catch (err) {
 		console.log(err);
 	}
 
 	try {
-    console.log("\n\nGet user by ID\n");
-		const result = await users.getUserById(firstUser._id);
-		console.log(result);
+		secondUser = await users.createUser("Jane", "Doe", "janedoe@email.com", 30, "Test@123", "0385943712");
+		console.log(secondUser);
+	} catch (err) {
+		console.log(err);
+	}
+
+	try {
+		thirdUser = await users.createUser("Derek", "Anderson", "anderson@email.com", 16, "Asd@123", "4950327384");
+		console.log(thirdUser);
+	} catch (err) {
+		console.log(err);
+	}
+
+	try {
+		fourthUser = await users.createUser("Joel", "Carrey", "joelcarrey@gmail.com", 13, "Qwe@123", "8946783471");
+		console.log(fourthUser);
+	} catch (err) {
+		console.log(err);
+	}
+
+	try {
+		fifthUser = await users.createUser("Gale", "Hyatt", "galehyatt@hotmail.com", 42, "Zxc@123", "9374856124");
+		console.log(fifthUser);
+	} catch (err) {
+		console.log(err);
+	}
+
+	try {
+		sixthUser = await users.createUser("Test", "Account", "testacc@email.com", 18, "Test@123", "4756283467");
+		console.log(sixthUser);
+	} catch (err) {
+		console.log(err);
+	}
+
+	try {
+		console.log("\n\nGet user by ID\n");
+		const result1 = await users.getUserById(firstUser._id);
+		const result2 = await users.getUserById(secondUser._id);
+		const result3 = await users.getUserById(thirdUser._id);
+		const result4 = await users.getUserById(fourthUser._id);
+		const result5 = await users.getUserById(fifthUser._id);
+		const result6 = await users.getUserById(sixthUser._id);
+
+		console.log(result1);
+		console.log(result2);
+		console.log(result3);
+		console.log(result4);
+		console.log(result5);
+		console.log(result6);
 	} catch (e) {
 		console.log(e);
 	}
 
 	try {
-		newJob = await jobs.createJob("Car Wash", "Need urgent car wash", "12th St", firstUser._id);
+		newJob = await jobs.createJob("Car Wash", "Need urgent car wash for my Toyota Highlander Hybrid. I will provide all cleaning supplies required for the task.", "12th St", firstUser._id);
+		console.log("The new job:");
 		console.log(newJob);
+		// console.log(await users.getUserById(firstUser));
 	} catch (e) {
 		console.log(e);
 	}
 
 	try {
-		const result = await users.getAllJobsByUser(firstUser._id);
+		console.log("All jobs by user");
+		const result = await users.getAllPostJobsById(firstUser._id);
 		console.log(result);
 	} catch (e) {
 		console.log(e);
@@ -58,14 +111,22 @@ async function main() {
 	//   console.log(e);
 	// }
 
+	// try {
+	// 	console.log("\n\n Search  Job:\n");
+	// 	const searchJob = await jobs.searchJobs("Car Wash");
+	// 	console.log(searchJob);
+	// } catch (e) {
+	// 	console.log(e);
+	// }
 
-	try {
-    console.log("\n\n Search  Job:\n");
-	  const searchJob = await jobs.searchJobs("Car Wash");
-	  console.log(searchJob);
-	} catch (e) {
-	  console.log(e);
-	}
+	// try {
+	// 	console.log("Create new comment");
+	// 	newComment = await comments.createComment(newJob._id, firstUser._id, `${firstUser.firstName} ${firstUser.lastName}`, "The pay for this job is fixed at $20");
+	// 	console.log(newComment);
+	// } catch (e) {
+	// 	console.log(e);
+	// }
+
 	await connection.closeConnection();
 }
 
