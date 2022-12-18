883406176841b171a8130685980518fd3716a920
Siddharth <sdalvi3@stevens.edu>
2022-12-17 19:56:03 -0500

    More client side validations

---
+++ public/js/resumeValidation.js
@@ -0,0 +1,33 @@
+
+// function checkResume(resume){
+//     if(!resume) throw "You must upload a resume to submit"
+//     if(!resume.includes("pdf")) throw "Only pdf is allowed"
+//     return resume
+// }
+
+// const staticForm1 = document.getElementById("resumeUpload")
+
+// if (staticForm1) {
+//     const Input = document.getElementById("resume").value
+
+//     // const errorContainer = document.getElementById('error-container1')
+//     // const errorTextElem = document.getElementsByClassName('text-goes-here')[0];
+
+//     staticForm1.addEventListener('submit', (event) => {
+//         try {
+//             // errorContainer.classList.add('hidden')
+            
+            
+//             if(Input == ""){
+//                 document.getElementById("resumeError").innerHTML = "You must upload a resume"
+//             } 
+//             if (resumeInput) {
+//                 errorContainer.style.display = "none"
+//             }
+//         } catch (e) {
+//             event.preventDefault();
+//             // errorTextElem.textContent = "Error: " + e
+//             // errorContainer.style.display = "block"
+//         }
+//     })
+// }
