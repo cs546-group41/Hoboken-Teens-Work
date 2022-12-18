09e117bb983dba18fd2040cde26c5819a742c24f
Siddharth <sdalvi3@stevens.edu>
2022-12-18 03:37:12 -0500

    Validations fix

--- public/js/resumeValidation.js
+++ public/js/resumeValidation.js
@@ -1,28 +1,39 @@
 
-function checkResume(resume){
-    if(!resume) throw "You must upload a resume to submit"
-    if(!resume.includes("pdf")) throw "Only pdf is allowed"
-    return resume
-}
+// function checkResume(resume){
+//     if(!resume) throw "You must upload a resume to submit"
+//     const re = /\.pdf/
+//     if(!re.exec(resume)){
+//         throw "Invalid file type please only upload pdf"
+//     }
+//     return resume
+// }
 
-const staticForm1 = document.getElementById("resumeUpload")
+// const staticForm1 = document.getElementById("resumeUpload")
+// const errorContainer = document.getElementById('error-container')
+// const errorTextElem = document.getElementsByClassName('text-goes-here')[0];
 
-if (staticForm1) {
-    const Input = document.getElementById("resume").value
+// if (staticForm1) {
+//     const Input1 = document.getElementById("resume")
+//     const Input = Input1.value;
+//     console.log(Input)
 
-
-    staticForm1.addEventListener('submit', (event) => {
-        try {
+//     staticForm1.addEventListener('submit', (event) => {
+//         try {
+//             function resume(input){
+//             const re = /.+\.(.pdf)$/i
+//             if(re.test(Input)) throw "File should be of type pdf"
+//             return input
+//             }
+//             const validatedResume = resume(Input)
+//             if(validatedResume){
+//                 errorContainer.style.display = "none"
+//             }
            
             
-            if(Input == ""){
-                document.getElementById("resumeError").innerHTML = "You must upload a resume"
-            } 
-            if (resumeInput) {
-                errorContainer.style.display = "none"
-            }
-        } catch (e) {
-            event.preventDefault();
-        }
-    })
-}
+//         } catch (e) {
+//             event.preventDefault();
+//             errorTextElem.textContent = "Error: " + e
+//             errorContainer.style.display = "block"
+//         }
+//     })
+// }
