2311d13b07d3f2aaeeac8911d6d6e355f301b876
Srif <530336037@qq.com>
2022-12-18 19:35:47 -0500

    comment jquery ajax

--- public/js/commentsValidation.js
+++ public/js/commentsValidation.js
@@ -1,4 +1,4 @@
-
+/*
 
 function checkComment(comment){
     if (!comment) throw "Must provide a comment";
@@ -31,4 +31,45 @@ if (staticForm) {
             errorContainer.style.display = "block"
         }
     })
-}
+}*/
+
+$(document).ready(function () {
+    $("#add-comment").submit(function(event){
+        event.preventDefault();
+        var comment =$('#comment').val().trim()
+        const jobId =$('#jobId').val().trim()
+        if (comment){
+            if ($.isNumeric(comment)) comment = comment.toString();
+            $('#error-container').hide();
+            $.ajax({
+                url:"addComment",
+                type:"POST",
+                contentType:'application/x-www-form-urlencoded',
+                data:{
+                    jobId: jobId,
+                    comment: comment
+                },
+                success:function(result){
+                    console.log(result)
+                    var DOMtext = $(`
+                        <li class="comment-listLi">
+                            <p>By: <a href="/user/${result.results.authorId}">${result.results.name}</a></p>
+                            <p>${result.results.comment}</p>
+                            <p>On: ${result.results.commentDate}</p>
+                            <br>
+                        </li>
+                        `)
+                    $('#comment-list').append(DOMtext)
+                },
+                error:function(){
+                    $('#error-container').show();
+                    $('#error-container').html('Failed to add comment!');
+                }
+            });
+        }
+        else{
+            $('#error-container').show();
+            $('#error-container').html('Cannot post a empty comment!');
+        }
+    });
+});
--- public/js/jobOperations.js
+++ public/js/jobOperations.js
@@ -1,3 +1,4 @@
+/*
 //add comment js funciton
 const addCommentForm = document.getElementById("add-comment")
 if(addCommentForm){
@@ -28,7 +29,7 @@ if(addCommentForm){
         xhttp.send(`jobId=${this.jobId.value}&comment=${this.comment.value}`);
     });
 }
-
+*/
 
 // //share job
 
