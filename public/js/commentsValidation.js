8cf81740c706dfe8a5f6b19e00e9e4c72a79df91
rishikiran2260039 <53871456+rishikiran2260039@users.noreply.github.com>
2022-12-19 06:49:17 +0530

    Add files via upload

--- public/js/commentsValidation.js
+++ public/js/commentsValidation.js
@@ -1,34 +1,40 @@
-
-
-function checkComment(comment){
-    if (!comment) throw "Must provide a comment";
-    return comment
-}
-
-
-const staticForm = document.getElementById("add-comment")
-
-if (staticForm) {
-    const Input = document.getElementById("comment")
-
-    const errorContainer = document.getElementById('error-container')
-    const errorTextElem = document.getElementsByClassName('text-goes-here')[0];
-
-    staticForm.addEventListener('submit', (event) => {
-        try {
-            errorContainer.classList.add('hidden')
-            const commentInput = Input.value;
-            const commentInputTrimmed = commentInput.trim()
-            if(commentInputTrimmed.length === 0) throw "Enter something in the comment box before clicking the submit button"
-            console.log(commentInput)
-            const validatedComment = checkComment(commentInput)
-            if (validatedComment) {
-                errorContainer.style.display = "none"
-            }
-        } catch (e) {
-            event.preventDefault();
-            errorTextElem.textContent = "Error: " + e
-            errorContainer.style.display = "block"
+$(document).ready(function () {
+    $("#add-comment").submit(function(event){
+        event.preventDefault();
+        var comment =$('#comment').val().trim()
+        const jobId =$('#jobId').val().trim()
+        if (comment){
+            if ($.isNumeric(comment)) comment = comment.toString();
+            $('#error-container').hide();
+            $.ajax({
+                url:"addComment",
+                type:"POST",
+                contentType:'application/x-www-form-urlencoded',
+                data:{
+                    jobId: jobId,
+                    comment: comment
+                },
+                success:function(result){
+                    console.log(result)
+                    var DOMtext = $(`
+                        <li class="comment-listLi">
+                            <p>By: <a href="/user/${result.results.authorId}">${result.results.name}</a></p>
+                            <p>${result.results.comment}</p>
+                            <p>On: ${result.results.commentDate}</p>
+                            <br>
+                        </li>
+                        `)
+                    $('#comment-list').append(DOMtext)
+                },
+                error:function(){
+                    $('#error-container').show();
+                    $('#error-container').html('Failed to add comment!');
+                }
+            });
         }
-    })
-}
+        else{
+            $('#error-container').show();
+            $('#error-container').html('Cannot post a empty comment!');
+        }
+    });
+});
--- public/js/resumeValidation.js
+++ public/js/resumeValidation.js
@@ -1,72 +1,116 @@
 
-const staticForm1 = document.getElementById("resumeUpload")
-const errorContainer = document.getElementById('error-container')
-const errorTextElem = document.getElementsByClassName('text-goes-here')[0];
+// function checkResume(resume){
+//     if(!resume) throw "You must upload a resume to submit"
+//     const re = /\.pdf/
+//     if(!re.exec(resume)){
+//         throw "Invalid file type please only upload pdf"
+//     }
+//     return resume
+// }
 
-if (staticForm1) {
-   
+// const staticForm1 = document.getElementById("resumeUpload")
+// const errorContainer = document.getElementById('error-container')
+// const errorTextElem = document.getElementsByClassName('text-goes-here')[0];
 
-    staticForm1.addEventListener('submit', (event) => {
+// if (staticForm1) {
+//     const Input1 = document.getElementById("resume")
+//     const Input = Input1.value;
+//     console.log(Input)
+
+//     staticForm1.addEventListener('submit', (event) => {
+//         try {
+//             function resume(input){
+//             const re = /.+\.(.pdf)$/i
+//             if(!re.test(Input)) throw "File should be of type pdf"
+//             return input
+//             }
+//             const validatedResume = resume(Input)
+//             // if(validatedResume){
+//             //     errorContainer.style.display = "none"
+//             // }
+//             if(!validatedResume){
+//                 document.getElementById("resumeError").innerHTML = "File should be of type pdf"
+
+//             }
+           
+            
+//             if(Input == ""){
+//                 document.getElementById("resumeError").innerHTML = "You must upload a resume"
+//             } 
+//             if (resumeInput) {
+//                 errorContainer.style.display = "none"
+//             }
+//         } catch (e) {
+//             event.preventDefault();
+//         }
+//     })
+// }
+// }*/
+//         } catch (e) {
+//             event.preventDefault();
+//             errorTextElem.textContent = "Error: " + e
+//             errorContainer.style.display = "block"
+//         }
+//     })
+// }
+/*
+$('#resumeUpload').submit((event) => {
+    const re = /.+\.(.pdf)$/i
+    const value =$('#resume').val().trim()
+    if ($('#resume').val().trim()) {
+      $('#error').hide();
+      $('#resume').focus();
+      if(!value.match(re)){
+        event.preventDefault();
+        $('#error').show();
+        $('#error').html('only pdf file is allowed');
+        $('#resume').focus();
+      }
+    }
+    else{
       event.preventDefault();
-
-        try {
-          const Input1 = document.getElementById("resume")
-          const Input = Input1.value;
-          const Inputtrim = Input.trim()
-          if(Inputtrim.length === 0) throw "upload something to submit"
-          else{
-            errorContainer.style.display = "none"
-        }
-        const re = /.+\.(.pdf)$/i
-        if(re.test(Inputtrim)){
-          errorContainer.style.display = "none"
-        }else throw "Upload format should be of a pdf file"
-          
-        } catch (e) {
-            event.preventDefault();
-            errorTextElem.textContent = "Error: " + e
-            errorContainer.style.display = "block"
-        }
-    })
-}
-
-
-
-
-
-
-
-
-// $('#resumeUpload').submit((event) => {
-//     const re = /.+\.(.pdf)$/i
-//     const value =$('#resume').val().trim()
-//     if ($('#resume').val().trim()) {
-//       $('#error').hide();
-//       $('#resume').focus();
-//     }
-//     else{
-//       $('#error').show();
-//       $('#error').html('You must upload something before submitting');
-//       $('#resume').focus();
-//     }
-//     if(!value.match(re)){
-//         event.preventDefault();
-//         $('#error').show();
-//         $('#error').html('only pdf file is allowed');
-//         $('#resume').focus();
-//       }
-
-//     // if(re.match($('#resume').val().trim())){
-//     //   $('#error1').hide();
-//     //   $('#resume').focus();
-//     // }else{
-//     //   event.preventDefault();
-//     //   $('#error1').show();
-//     //   $('#error1').html('only pdf file is allowed');
-//     //   $('#resume').focus();
-//     // }
-//     if ($('#resume').val().trim()) {
-//         $('#error1').hide();
-//         $('#resume').focus();
-//       }
-//   });
+      $('#error').show();
+      $('#error').html('You must upload something before submitting');
+      $('#resume').focus();
+    }
+    // if(re.match($('#resume').val().trim())){
+    //   $('#error1').hide();
+    //   $('#resume').focus();
+    // }else{
+    //   event.preventDefault();
+    //   $('#error1').show();
+    //   $('#error1').html('only pdf file is allowed');
+    //   $('#resume').focus();
+    // }
+    //if ($('#resume').val().trim()) {
+    //    $('#error1').hide();
+    //    $('#resume').focus();
+    //  }
+  });
+*/
+$(document).ready(function(){
+  $('input[type="file"]').change(function(e){
+      const re = /(pdf)$/i
+      var fileName = e.target.files[0].name;
+      var fileType = fileName.split(".")
+      fileType = fileType[fileType.length-1]
+      var fileSize = e.target.files[0].size
+      if(!re.test(fileType)){
+          $('#applyJobButton').hide();
+          $('#resumeError').show();
+          $('#resumeError').html('Only pdf file is allowed');
+          $('#resume').focus()   
+      }
+      else if (fileSize > 4*1024*1024){
+          $('#applyJobButton').hide();
+          $('#resumeError').show();
+          $('#resumeError').html('Choiced File Exceed size limit!');
+          $('#resume').focus()
+      }
+      else{
+        $('#applyJobButton').show();
+        $('#resumeError').hide();
+        $('#resume').focus();
+      }
+  });
+});
